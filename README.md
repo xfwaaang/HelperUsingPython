80dbf99a71b3a8866f7d91a6563ab84294d67175
xiaofeng <xfwangmm@gmail.com>
2018-07-04 22:15:18 +0800

    add watermark

--- README.md
+++ README.md
@@ -4,9 +4,7 @@
 - 批量重命名文件
 
 ```
-命令行执行
 PS E:\WorkSpace> python batch_rename_file.py E:\WorkSpace\test
-运行结果
 E:\WorkSpace\test\3d.txt -> E:\WorkSpace\test\第1章.txt
 E:\WorkSpace\test\bb.txt -> E:\WorkSpace\test\第2章.txt
 E:\WorkSpace\test\ds.txt -> E:\WorkSpace\test\第3章.txt
@@ -18,19 +16,20 @@ E:\WorkSpace\test\tt.txt -> E:\WorkSpace\test\第8章.txt
 E:\WorkSpace\test\vv.txt -> E:\WorkSpace\test\第9章.txt
 ```
 
-- PDF操作
+- 拼接给定的PDF文件
+    
 ```
-拼接给定的PDF文件
 PS E:\WorkSpace\HelperUsingPython> python pdf_helper.py merge E:\WorkSpace\HelperUsingPython\test\test.pdf E:\WorkSpace\HelperUsingPython\test\test-1.pdf
-运行结果
 There are 2 pdfs to be merged
 merging E:\WorkSpace\HelperUsingPython\test\test.pdf
 merging E:\WorkSpace\HelperUsingPython\test\test-1.pdf
 save the merged pdf to E:\WorkSpace\HelperUsingPython\test\merge-1530680353.pdf
+```
+    
+- 拼接给定目录下的所有PDF文件
 
-拼接给定目录下的所有PDF文件
+```
 PS E:\WorkSpace\HelperUsingPython> python pdf_helper.py merge_all E:\WorkSpace\HelperUsingPython\test
-运行结果
 There are 5 pdfs to be merged
 merging E:\WorkSpace\HelperUsingPython\test\hhh.pdf
 merging E:\WorkSpace\HelperUsingPython\test\merge-1530680353.pdf
@@ -38,10 +37,12 @@ merging E:\WorkSpace\HelperUsingPython\test\test - 副本.pdf
 merging E:\WorkSpace\HelperUsingPython\test\test-1.pdf
 merging E:\WorkSpace\HelperUsingPython\test\test.pdf
 save the merged pdf to E:\WorkSpace\HelperUsingPython\test\merge-1530680513.pdf
+```
 
-分割给定的PDF文件
+- 分割给定的PDF文件
+
+```
 PS E:\WorkSpace\HelperUsingPython> python pdf_helper.py split E:\WorkSpace\HelperUsingPython\test\ggg.pdf 4 10 15
-运行结果
 spliting pdf E:\WorkSpace\HelperUsingPython\test\ggg.pdf to 4 parts 
 save the splited pdf from page 0 to 4 to E:\WorkSpace\HelperUsingPython\test\ggg--0-4.pdf
 save the splited pdf from page 5 to 10 to E:\WorkSpace\HelperUsingPython\test\ggg--5-10.pdf
@@ -49,14 +50,24 @@ save the splited pdf from page 11 to 15 to E:\WorkSpace\HelperUsingPython\test\g
 save the splited pdf from page 16 to 20 to E:\WorkSpace\HelperUsingPython\test\ggg--16-20.pdf
 ```
 
+- 添加文字水印
+
+```
+PS E:\WorkSpace\HelperUsingPython> python pdf_helper.py mark_word E:\WorkSpace\HelperUsingPython\test\IDN_XD.pdf 'hello world!'
+the watermark is saved as E:\WorkSpace\HelperUsingPython\test\water_mark-1530712159.pdf
+the watermarked pdf is saved as E:\WorkSpace\HelperUsingPython\test\IDN_XD--watermarked-1530712160.pdf
+```
+
+![文字水印](/files/文字水印.png)
+![添加过文字水印的PDF](/files/文字水印PDF.png)
+
 ### ToDo
 - [x] 批量重命名文件
 - [ ] 批量下载百度图片 
 - [ ] 批量下载视频
 - [ ] PDF操作
     - [x] 拼接，分割
-    - [ ] 加水印，去水印
+    - [x] 加水印
     - [ ] 转Word或文本
-    - [ ] 英文->中文
-- [ ] word文档操作--转PDF或文本
+- [ ] Word文档操作--转PDF或文本
 - [ ] Excel表格操作
--- pdf_helper.py
+++ pdf_helper.py
@@ -5,6 +5,8 @@
 # @email   : xfwangmm@gmail.com
 
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
+from reportlab.pdfgen import canvas 
+from reportlab.lib.units import cm 
 import os
 import sys
 import time
@@ -102,6 +104,81 @@ class PDFHelper(object):
 			start = index + 1;
 		pass
 
+	def create_word_mark(content, path):
+		"""
+		create word watermark and save it to the directory of the pdf to be added watermark
+		content: the words of watermark
+		path: the absolute path of the pdf to be added watermark
+		"""
+	    #默认大小为21cm*29.7cm
+		mark_pdf = os.path.join(os.path.split(path)[0], 'water_mark-' + str(int(time.time())) + '.pdf')
+		c = canvas.Canvas(mark_pdf, pagesize = (30*cm, 30*cm))
+		#移动坐标原点(坐标系左下为(0,0))
+		c.translate(5*cm, 10*cm)
+		                                                                                                                        
+		#设置字体
+		c.setFont("Helvetica", 50)
+		# #指定描边的颜色
+		# c.setStrokeColorRGB(1, 1, 1)
+		# #指定填充颜色
+		# c.setFillColorRGB(1, 1, 1)
+		# #画一个矩形
+		# c.rect(cm, cm, 7*cm, 7*cm, fill=1)
+		                                                                                                                        
+		#旋转25度，坐标系被旋转
+		c.rotate(25)
+		#指定填充颜色
+		c.setFillColorRGB(0.6, 0.5, 0.4)
+		#设置透明度，1为不透明
+		# c.setFillAlpha(0.3)
+		#画几个文本，注意坐标系旋转的影响
+		# c.drawString(3*cm, 0*cm, content)
+		c.setFillAlpha(0.6)
+		c.drawString(6*cm, 3*cm, content)
+		# c.setFillAlpha(1)
+		# c.drawString(9*cm, 6*cm, content)
+		                                                                                                                        
+		#关闭并保存pdf文件
+		c.save()
+		return mark_pdf
+
+	def add_mark(pdf, mark_pdf):
+		"""
+		add watermark
+		"""
+		# 获取PDF文件所在目录和PDF文件名
+		pdf_dir = os.path.split(pdf)[0]
+		pdf_name = os.path.splitext(os.path.split(pdf)[1])[0]
+
+		pdf_reader = PdfFileReader(pdf)
+		mark_pdf_reader = PdfFileReader(mark_pdf)
+		mark_page = mark_pdf_reader.getPage(0)
+		page_num = pdf_reader.getNumPages()
+		pdf_writer = PdfFileWriter()
+		for i in range(0, page_num):
+			page = pdf_reader.getPage(i)
+			page.mergePage(mark_page)
+			pdf_writer.addPage(page)
+		out_file = os.path.join(pdf_dir, pdf_name + '--' + 'watermarked-' + str(int(time.time())) + '.pdf')
+		pdf_writer.write(open(out_file, 'wb'))
+		print('the watermarked pdf is saved as ' + str(out_file))
+		pass
+
+	def create_image_mark(image, path):
+		"""
+		create image watermark and save it to the directory of the pdf to be added watermark
+		image: the image of watermark
+		path: the absolute path of the pdf to be added watermark
+		"""
+		mark_pdf = os.path.join(os.path.split(path)[0], 'water_mark-' + str(int(time.time())) + '.pdf')                                                                               
+		c = canvas.Canvas(mark_pdf, pagesize = (20*cm, 20*cm))
+		c.setFillAlpha(0.4) #设置透明度
+		c.rotate(25)
+		c.translate(5*cm, 5*cm)
+		c.drawImage(image, 7*cm, 7*cm, 6*cm, 6*cm)    #这里的单位是物理尺寸
+		c.save()
+		return mark_pdf
+
 key = sys.argv[1]
 if key == 'merge':
 	PDFHelper.merge(sys.argv[2:])
@@ -109,7 +186,14 @@ elif key == 'merge_all':
 	PDFHelper.merge_all(sys.argv[2])
 elif key == 'split':
 	PDFHelper.split(sys.argv[2], sys.argv[3:])
-	pass
+elif key == 'mark_word':
+	mark_pdf = PDFHelper.create_word_mark(sys.argv[3], sys.argv[2])
+	print('the watermark is saved as ' + str(mark_pdf))
+	PDFHelper.add_mark(sys.argv[2], mark_pdf)
+elif key == 'mark_image':
+	mark_pdf = PDFHelper.create_image_mark(sys.argv[3], sys.argv[2])
+	print('the watermark is saved as ' + str(mark_pdf))
+	PDFHelper.add_mark(sys.argv[2], mark_pdf)
 else:
 	print('input error!!!')
 	
